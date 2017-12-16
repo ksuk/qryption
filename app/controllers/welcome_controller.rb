22090e9d4967d2aa58fb57fd0625d0fead69ea30
sqrtxx <sqrtxx@gmail.com>
2017-12-16 21:16:16 +0900

    Fix layout

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     return render 'unlogin' unless current_user
-
+    @questions_top_5 = Question.top_5
     @questions = Question.all
   end
 end
