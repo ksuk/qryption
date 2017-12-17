4f7046c7de4edccd4beed73373d53172fdcc754f
sqrtxx <sqrtxx@gmail.com>
2017-12-17 21:28:23 +0900

    Create: Question spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,8 +3,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-require 'support/devise'
-require 'support/factory_bot'
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
@@ -12,4 +11,5 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+  config.include RequestSpecHelper, type: :request
 end
