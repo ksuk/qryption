cfdac6fbf7f01236a57ee0f76b87c1ea9c633271
kyohei0423 <kyo199333@gmail.com>
2018-02-05 18:34:33 +0900

    Change root path to questions#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'questions#index'
-  resources :welcome, only: [:index]
+  resource :welcome, only: [:index]
   resources :questions, only: [:new, :create, :show]
   resources :answers, only: [:create]
   devise_for :users, controllers: {
