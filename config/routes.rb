241581037a16aef203ab77fed3eb29ce50158b2f
kyohei0423 <kyo199333@gmail.com>
2018-02-06 14:02:25 +0900

    Modify routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'questions#index'
-  resource :welcome, only: [:index]
+  resources :top, only: :index
   resources :questions, only: [:new, :create, :show]
   resources :answers, only: [:create]
   devise_for :users, controllers: {
