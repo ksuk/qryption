a4e3e129f903148c184a58fe253f18fbedc3f2dc
kyohei0423 <kyo199333@gmail.com>
2018-02-13 17:43:30 +0900

    Remove title column from questions table

--- db/migrate/20171216025731_create_questions.rb
+++ db/migrate/20171216025731_create_questions.rb
@@ -2,7 +2,6 @@ class CreateQuestions < ActiveRecord::Migration[5.1]
   def change
     create_table :questions, options: "ENGINE=InnoDB ROW_FORMAT=DYNAMIC CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci" do |t|
       t.references :user, null: false, index: true
-      t.string :title, null: false, index: true
       t.text :body, null: false
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -44,11 +44,9 @@ ActiveRecord::Schema.define(version: 20171216030601) do
 
   create_table "questions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
     t.bigint "user_id", null: false
-    t.string "title", null: false
     t.text "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["title"], name: "index_questions_on_title"
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
