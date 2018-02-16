8441ae1af14260b4de00981492e7c09fc5c40150
kyohei0423 <kyo199333@gmail.com>
2018-02-16 16:41:27 +0900

    Set the number per page

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,8 @@ class Question < ApplicationRecord
   belongs_to :user
   has_many :answers, dependent: :destroy
 
+  paginates_per 20
+
   validates :body, presence: true
 
   scope :top_5, -> { where(nil) }
