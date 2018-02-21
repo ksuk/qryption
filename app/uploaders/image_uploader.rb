b8a1733f8c42c8e651c71594530e83e586f4f03a
kyohei0423 <kyo199333@gmail.com>
2018-02-21 17:13:00 +0900

    Generate uploader

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,27 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  case Rails.env
+  when 'production'
+    storage :fog
+  else
+    storage :file
+  end
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_whitelist
+    %w(jpg jpeg png)
+  end
+
+  def filename
+    "something.jpg" if original_filename
+  end
+
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+  end
+end
