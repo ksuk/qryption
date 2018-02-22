a4914c681e6a550c8d2a8b2840b09a5317c10cb2
kyohei0423 <kyo199333@gmail.com>
2018-02-22 17:48:14 +0900

    Implement update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,25 @@ class UsersController < ApplicationController
   def show
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
+    if current_user.update(user_params)
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user)
+          .permit(
+            :name,
+            :email,
+            :profile,
+            :website,
+            :image)
   end
 end
