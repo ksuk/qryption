3eb512878670c22470f4baa7d727d16c84e47341
kyohei0423 <kyo199333@gmail.com>
2018-02-22 22:23:14 +0900

    Add condition

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,17 @@
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AWS_S3_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_S3_SECRET_ACCESS_KEY'],
-    region: 'ap-northeast-1',
-    host: 's3.example.com',
-    endpoint: 'https://s3.example.com:8080'
-  }
-  config.cache_storage = :fog
-  config.fog_directory  = 'qryption'
-  config.fog_public = false
-  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+  if Rails.env == 'production'
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_S3_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_S3_SECRET_ACCESS_KEY'],
+      region: 'ap-northeast-1',
+      host: 's3.example.com',
+      endpoint: 'https://s3.example.com:8080'
+    }
+    config.cache_storage = :fog
+    config.fog_directory  = 'qryption'
+    config.fog_public = false
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+  end
 end
