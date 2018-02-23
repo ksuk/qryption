c4be7ccda930e586def1e5ffa62c2350fffe3699
kyohei0423 <kyo199333@gmail.com>
2018-02-23 14:24:38 +0900

    Add config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,14 @@
 CarrierWave.configure do |config|
-  if Rails.env == 'production'
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_S3_KEY_ID'],
       aws_secret_access_key: ENV['AWS_S3_SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1'
+      region: 'ap-northeast-1',
+      path_style: true
     }
     config.cache_storage = :fog
     config.fog_directory  = 's3-for-qryption/qryption'
     config.fog_public = false
     config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
-  end
 end
