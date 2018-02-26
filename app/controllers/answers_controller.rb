914300cd3077795a6574fe6a0fb8d78a3fd5ab25
kyohei0423 <kyo199333@gmail.com>
2018-02-26 16:18:24 +0900

    Implement create answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,16 +1,34 @@
 class AnswersController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.build
+    rescue
+      redirect_to root_path, warning: '申し訳ございません。お探しの質問は見つかりませんでした。'
+  end
+
   def create
     @answer = current_user.answers.build(answer_params)
     if @answer.save
-      redirect_to question_path(@answer.question)
+      redirect_to question_path(@answer.question), success: '回答を作成しました。'
     else
-      redirect_to question_path(@answer.question)
+      @question = Question.find(params[:question_id])
+      render :new
     end
+    rescue
+      redirect_to root_path, warning: '申し訳ございません。お探しの質問は見つかりませんでした。'
   end
 
   private
 
+  def set_question
+    @question = Question.find(params[:question_id])
+  end
+
   def answer_params
-    params.require(:answer).permit(:title, :body, :question_id)
+    params.require(:answer)
+          .permit(:body)
+          .merge(question_id: params[:question_id])
   end
 end
